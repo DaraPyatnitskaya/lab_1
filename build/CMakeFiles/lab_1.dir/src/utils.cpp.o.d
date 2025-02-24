198e9138d23c094b209ccfdd5677c4d5332d35a1
DaraPyatnitskaya <a.dahsik.a@gmail.com>
2025-02-24 16:45:32 +0300

    Initial commit: CMake project setup

---
+++ build/CMakeFiles/lab_1.dir/src/utils.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/lab_1.dir/src/utils.cpp.o: \
+  /Users/darasatalova/Desktop/lab_1/src/utils.cpp
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int add(int a, int b);
+int sub(int a, int b);
+
+int main(){
+  int a = 10;
+  int b = 20;
+  int summ = add(a,b);
+  int nesumm = sub(a,b);
+  std::cout << summ << std::endl << nesumm << std::endl;
+  }
