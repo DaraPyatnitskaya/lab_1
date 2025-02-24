8e59a3b089b4e6270a2e8327e5e3d9f559050f5f
DaraPyatnitskaya <a.dahsik.a@gmail.com>
2025-02-24 16:54:41 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 
 int add(int a, int b);
 int sub(int a, int b);
+int ymnoz(int a, int b);
 
 int main(){
   int a = 10;
   int b = 20;
   int summ = add(a,b);
   int nesumm = sub(a,b);
-  std::cout << summ << std::endl << nesumm << std::endl;
+  int mul = ymnoz(a,b);
+  std::cout << summ << std::endl << nesumm << std::endl << mul << std::endl;
   }
