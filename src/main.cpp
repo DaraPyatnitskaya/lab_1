5ddeceb47e15bb7306933e84efaa11dc2d466ac6
DaraPyatnitskaya <a.dahsik.a@gmail.com>
2025-02-24 17:19:27 +0300

    Merge branch 'feature-utils'
    Conflicts:
    src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,18 @@
 
 int add(int a, int b);
 int sub(int a, int b);
+int ymnoz(int a, int b);
 
 int main(){
   int a = 10;
   int b = 20;
   int summ = add(a,b);
   int nesumm = sub(a,b);
+  int mul = ymnoz(a,b);
+
   std::cout << "Updated summ: " << summ;
   std::cout << "Updated nesumm: " << nesumm;
-  }
+  std::cout << "Updated mul: " << mul;
+
+  return 0;
+}
